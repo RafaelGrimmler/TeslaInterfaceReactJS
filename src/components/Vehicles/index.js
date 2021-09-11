2674a4a14df22c353afdff2811ff43e480de1c2a
Rafael Grimmler da Rocha <grimmlerr@gmail.com>
2021-09-11 20:17:58 -0300

    layoutDone

--- src/components/Vehicles/index.js
+++ src/components/Vehicles/index.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import ListVehicles from '../../ListVehicles.json';
 import Vehicle from '../Vehicle'
 
-import { Container, LayoutName } from './styles';
+import { Container, LayoutName, LayoutButtonsContainer ,LayoutButtons, Arrow, ArrowIcon } from './styles';
 
 function Vehicles() {
 
     const [currentVehicle, setCurrentVehicle] = useState(0)
     const [opacity, setOpacity] = useState(1)
+    const [arrowAnimationController, setArrowAnimationController] = useState(false)
+    const [opacityArrow, setOpacityArrow] = useState(1)
     const opacityPalette = [0.9,0.7,0.4,0.2,0,0,0.2,0.4,0.7,0.9]
 
     const scroll = e => {
@@ -27,9 +29,20 @@ function Vehicles() {
         return
       }
 
-      // Define a opacidade com base na paleta de opacidade
       setOpacity(opacityPalette[floatPart.toFixed(1) * 10 - 1])
+
+      // Define opacidade do ArrowIcon
+      if( currentView > 0.5 ){
+        setOpacityArrow(0)
+      }else{
+        setOpacityArrow(opacity)
+      }
+
     }
+
+    window.setTimeout(() => {
+      setArrowAnimationController(true)
+    }, 2500);
 
     return (
       <Container onScroll={scroll}>
@@ -40,9 +53,26 @@ function Vehicles() {
                 {ListVehicles[currentVehicle].text}
                 <a href="https://www.tesla.com/">
                   {ListVehicles[currentVehicle].link ? ListVehicles[currentVehicle].link :  ''}
-                  </a>
+                </a>
               </h2>
           </LayoutName>
+          <LayoutButtonsContainer opacity={opacity}>
+            <LayoutButtons 
+              color='#FFFFFF' 
+              backgroundColor='#393C41'>
+              <a href="https://www.tesla.com/">CUSTOM ORDER</a>
+            </LayoutButtons>
+            <LayoutButtons 
+              color='#393C41'
+              backgroundColor='#FFFFFF'>
+              <a href="https://www.tesla.com/">EXISTING INVENTORY</a>
+            </LayoutButtons>
+          </LayoutButtonsContainer>
+          <Arrow 
+            repeat={arrowAnimationController}
+            opacity={opacityArrow}>
+            <ArrowIcon />
+          </Arrow>
           {
               ListVehicles.map((item, key)=>
                 <Vehicle key={key} src={item.src} />
--- src/components/Vehicles/styles.js
+++ src/components/Vehicles/styles.js
@@ -1,4 +1,5 @@
 import styled, { keyframes } from 'styled-components';
+import { IoIosArrowDown } from 'react-icons/io'
 
 export const Container = styled.div`
     overflow-x: hidden;
@@ -19,6 +20,39 @@ const StartPageSubTitle = keyframes`
     0%{opacity: 0;}
     80%{opacity: 0;}
     100%{opacity: 1;}
+`;
+
+const StartPageArrow = keyframes`
+    0%{
+        bottom: 40px;
+        opacity: 0;
+    }
+    80%{
+        bottom: 40px;
+        opacity: 0;
+    }
+    100%{
+        bottom: 15px;
+        opacity: 1;
+    }
+`;
+
+const StartPageArrowIcon = keyframes`
+    0%{
+        bottom: 15px;
+    }
+    70%{
+        bottom: 15px
+    }
+    80%{
+        bottom: 12px;
+    }
+    90%{
+        bottom: 17px;
+    }
+    100%{
+        bottom: 15px;
+    }
 `;
 
 export const LayoutName = styled.div`
@@ -56,3 +90,52 @@ export const LayoutName = styled.div`
         font-weight: 500;
     }
 `;
+
+export const LayoutButtonsContainer = styled.div`
+    position: fixed;
+    bottom: 80px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: ${props=>props.opacity};
+    text-align: center;
+    height: 40px;
+    display: flex;
+    animation: ${StartPageSubTitle} 2s;
+`;
+
+export const LayoutButtons = styled.div`
+    height: 100%;
+    width: 255px;
+    background-color: ${props=>props.backgroundColor};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 90px;
+    margin: 0 12.5px;
+    cursor: pointer;
+
+    & a {
+        text-decoration: none;
+        padding: 12px 20px;
+        color: ${props=>props.color};
+    }
+`;
+
+export const Arrow = styled.div`
+    position: fixed;
+    bottom: 15px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: ${props=>props.opacity};
+    text-align: center;
+    height: 40px;
+    display: flex;
+    
+    animation: ${props => props.repeat ? StartPageArrowIcon : StartPageArrow} ${props => props.repeat ? '2s infinite' : '2.5s'};
+    
+`;
+
+export const ArrowIcon = styled(IoIosArrowDown)`
+    font-size: 40px;
+`;
+
